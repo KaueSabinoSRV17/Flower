e2945a0aff01bfcea6765e5b0b17322055ba6dd2
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-05-01 13:35:52 -0300

    feat: initial cobra cli setup

---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+/*
+Copyright © 2023 Kauê Sabino Villaça de Almeida kauevillaca2018@gmail.com
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "Flower",
+	Short: "A CLI tool to streamline the use of Git Flow and Conventional Commits.",
+	Long: `
+  Flower is a CLI tool developers that follow the patterns of Git Flow and Conventional Commits.
+  
+  For example, you could automatically prefix each commit with a Conventional Commit and make a Git Flow
+  branch structure`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.Flower.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
