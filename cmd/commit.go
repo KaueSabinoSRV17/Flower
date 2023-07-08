90346f04e8fc1f76b7e993431097baf00b317760
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-07-08 19:08:53 -0300

    chore: removing cobra comments

--- cmd/commit.go
+++ cmd/commit.go
@@ -14,7 +14,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// commitCmd represents the commit command
 var commitCmd = &cobra.Command{
 	Use:   "commit",
 	Short: "Commits always prefixing with a conventional commit",
@@ -56,14 +55,4 @@ var commitCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(commitCmd)
 	commitCmd.Flags().BoolP("push", "p", false, "Push changes after commit is completed")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// commitCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// commitCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
